ce157d15bda5c6e93784224ddf4a3de396ac5703
Joost de Valk <joost@yoast.com>
2012-09-29 15:43:14 +0200

    New way of doing config, some minor fixes.

--- README.markdown
+++ README.markdown
@@ -5,3 +5,7 @@ This simple PHP script is meant to help you easily create XML Sitemaps for stati
 ## Configuration
 
 Open the PHP file and configure the settings until it says "STOP EDITING HERE". It's that simple.
+
+## Changelog
+
+2012-09-29: move configuration to config.php, fix outputted URLs
---
+++ config-sample.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Change the configuration below and rename this file to config.php
+ */
+
+// The directory to check, this script doesn't work recursively. Make sure the DIR ends ups in the Sitemap URL Dir below, otherwise the links to files will be broken!
+define( 'SITEMAP_DIR', './' );
+
+// With trailing slash!
+define( 'SITEMAP_DIR_URL', 'http://www.example.com/' );
+
+// The file types, you can just add them on, so 'pdf', 'php' would work
+$filetypes = array( 'php', 'html', 'pdf' );
+
+// The replace array, this works as file => replacement, so 'index.php' => '', would make the index.php be listed as just /
+$replace = array( 'index.php' => '' );
+
+// The XSL file used for styling the sitemap output, make sure this path is relative to the root of the site.
+$xsl = 'xml-sitemap.xsl';
+
+// The Change Frequency for files, should probably not be 'never', unless you know for sure you'll never change them again.
+$chfreq = 'never';
+
+// The Priority Frequency for files. There's no way to differentiate so it might just as well be 1.
+$prio = 1;
+
+// Ignore array, all files in this array will be: ignored!
+$ignore = array( 'config.php' );
